9600600e0d0ed4a1afec12a7e131ffaa1251d07a
fitsum <fitsumv7@gmail.com>
2024-10-17 01:46:54 +0300

    add advanced pro

---
+++ script.js
@@ -0,0 +1,44 @@
+  document.addEventListener('DOMContentLoaded', function () {
+  const addButton = document.getElementById("add-task-btn");
+  const taskInput = document.getElementById('task-input');
+  const taskList = document.getElementById("task-list");
+
+  function addTask() {
+    var taskText = taskInput.value.trim();
+    if (taskText === "") {
+      alert('Enter a task');
+      return;
+    }
+      var List = document.createElement('li');
+      List.textContent = taskText;
+          
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'Remove';
+      removeButton.className = 'remove-btn';
+       
+      removeButton.addEventListener('click', () => {
+        List.remove();
+      })
+
+      List.appendChild(removeButton);
+      taskList.appendChild(List);
+      taskInput.value = "";
+    }
+
+    addButton.addEventListener('click', () => {
+      addTask();
+    })
+
+    taskInput.addEventListener('keypress', (event) => {
+      if (event.key == 'Enter') {
+        addTask();
+      }
+    })
+  
+
+  }
+)
+
+
+
+
